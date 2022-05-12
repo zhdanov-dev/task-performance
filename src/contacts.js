0e6d369b5c6b8b472395e0c565868a49ab57155f
zhdanushka <71259608+zhdanushka@users.noreply.github.com>
2022-05-12 11:19:54 +0500

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,26 @@
+const contacts = document.getElementsByClassName("contacts")[0];
+const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
+
+function addContacts() {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < 50000; i++) {
+    const child = document.createElement("div");
+    child.textContent = i;
+    child.classList.add("contact");
+    fragment.appendChild(child);
+  }
+  contacts.appendChild(fragment);
+}
+
+contacts.addEventListener("scroll", (e) => {
+  const items = Array.from(contacts.getElementsByClassName("contact"));
+  const itemOffsets = items.map((item) => item.offsetTop);
+  const topItemIndex = itemOffsets.findIndex(
+    (offset) => contacts.scrollTop - offset <= -18
+  );
+  if (topItemIndex !== -1) {
+    stickyHeader.textContent = items[topItemIndex].textContent;
+  }
+});
+
+addContacts();
